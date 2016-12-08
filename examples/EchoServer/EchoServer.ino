f2f519749ca619fa7e0f7601d804eb6f3be6b448
cassy73 <cassy@zalaszam.hu>
2016-12-08 15:03:55 +0100

    Add files via upload

--- examples/EchoServer/EchoServer.ino
+++ examples/EchoServer/EchoServer.ino
@@ -22,12 +22,13 @@
 // The connection_data struct needs to be defined in an external file.
 #include <UIPServer.h>
 #include <UIPClient.h>
+#include "utility/logging.h"
 
 EthernetServer server = EthernetServer(1000);
 
 void setup()
 {
-  Serial.begin(9600);
+  LogObject.begin(9600);
 
   uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
   IPAddress myIP(192,168,0,6);
@@ -49,7 +50,7 @@ void loop()
             {
               uint8_t* msg = (uint8_t*)malloc(size);
               size = client.read(msg,size);
-              Serial.write(msg,size);
+              LogObject.write(msg,size);
               client.write(msg,size);
               free(msg);
             }
